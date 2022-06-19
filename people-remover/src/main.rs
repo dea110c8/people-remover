8cb46378645a1f1a4a7a30e4b1a4bcd21b1c3f3a
dea110c8 <dea110c8@protonmail.com>
2022-06-19 21:21:06 +0000

    removed exports

--- people-remover/src/main.rs
+++ people-remover/src/main.rs
@@ -12,5 +12,19 @@ fn main() {
         eprintln!("Not enough valid inputs to continue.");
     }
 
-    let inputs = inputs.iter().map(|path| primitives::load_image(path));
+    let inputs = inputs
+        .iter()
+        .map(|path| primitives::load_image(path))
+        .collect::<Result<Vec<_>, _>>();
+
+    if let Err(error) = inputs {
+        eprintln!("Failed to load one of the input images: {}", error);
+        return;
+    }
+
+    // transform the inputs so that we can ensure they are similar
+    let thumbnails = inputs.unwrap().iter().map(|image| {
+        let dim = image.width().min(image.height());
+        dim
+    });
 }
