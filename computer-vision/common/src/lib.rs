565ff9da8c505e3d792ac1ec5910d8c2b882ebec
dea110c8 <dea110c8@protonmail.com>
2022-06-19 21:20:44 +0000

    added matrix data to images

--- computer-vision/common/src/lib.rs
+++ computer-vision/common/src/lib.rs
@@ -1,7 +1,15 @@
 mod error;
+mod matrix;
+mod numerics;
 pub use error::Error;
+pub use matrix::Matrix;
+pub use numerics::NumericType;
 
 pub trait Image {
+    type PixelFormat;
+
     fn width(&self) -> u32;
     fn height(&self) -> u32;
+
+    fn get_matrix(&self) -> &matrix::Matrix<Self::PixelFormat> where <Self as Image>::PixelFormat : NumericType;
 }
